03635abd139a0a7326cc7f1f93d145271455b2d1
FrancescoER <ferc26899@gmail.com>
2022-05-24 01:09:36 -0400

    menu movil terminado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import "./app.scss"
+//SCSS
+import "./sass/app.scss"
+//Imagenes
 import yo from "./img/yo.jpg"
+import bg_header from "./img/bg_header.jpg"
+import bg_contac from "./img/bg_contac.jpg"
+//JavaScript
+import "./js/toggleMenu"
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         use: ["babel-loader"],
       },
       {
-        test:/\.s[ac]ss$/i,
+        test:/\.scss$/i,
         use: [
           "style-loader",
           "css-loader",
